2a48fdfb01d0aba646981edcd113d6b7754425d1
Sebastian <ingsebastian.rucci@gmail.com>
2024-10-30 23:46:52 -0300

    agregado de configuracion de css

--- js/scripts.js
+++ js/scripts.js
@@ -19,3 +19,18 @@ addToCartButton.addEventListener('click', () => {
     cartCount++; // Incrementar el contador
     itemCount.textContent = cartCount; // Actualizar el valor del span con el nuevo contador
 });
+
+
+document.getElementById("myForm").addEventListener("submit", function(event) {
+    const captchaCheckbox = document.getElementById("captchaCheckbox");
+    const errorMessage = document.getElementById("errorMessage");
+
+    // Verificamos si el captcha está marcado
+    if (!captchaCheckbox.checked) {
+        event.preventDefault(); // Evita el envío del formulario
+        errorMessage.style.display = "block"; // Muestra el mensaje de error
+    } else {
+        errorMessage.style.display = "none"; // Oculta el mensaje de error si está marcado
+        alert("Formulario enviado correctamente.");
+    }
+});
